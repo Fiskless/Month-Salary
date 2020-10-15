1987b83220c24a8e3db67d47c985cdbb785310ef
Fiskless <70478482+Fiskless@users.noreply.github.com>
2020-10-15 14:59:56 +0500

    Simplified the code, removed an additional level of abstraction(page)

--- main.py
+++ main.py
@@ -5,117 +5,95 @@ from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
 
-def predict_average_rub_salary_per_page_hh(programming_language, page=0):
+def predict_average_rub_salary_hh(programming_language):
 
-    url = f'https://api.hh.ru/vacancies'
-    params = {
-        'text': f'Программист{programming_language}',
-        'period': '30',
-        'area': '1',
-        'page': page
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    sum_salary, error, predicted_salary, vacancy_number = 0, 0, 0, 0
-    resp = response.json()
-    vacancies = resp['items']
-    for vacancy_number, vacancy in enumerate(vacancies):
-        try:
-            lower_salary = vacancies[vacancy_number]['salary']['from']
-            top_salary = vacancies[vacancy_number]['salary']['to']
-            currency = vacancies[vacancy_number]['salary']['currency']
-            predicted_salary, error = calculate_predict_salary(
-                currency,
-                lower_salary,
-                top_salary,
-                'RUR',
-                error
-            )
-            sum_salary += predicted_salary
-        except TypeError:
-            error = error + 1
-    number_of_pages = resp['pages']
-    vacancy_numbers_per_page = vacancy_number + 1 - error
-    vacancies_found = resp['found']
-    try:
-        average_salary_per_page = int(sum_salary/vacancy_numbers_per_page)
-    except ZeroDivisionError:
-        average_salary_per_page = 0
-    return (vacancy_numbers_per_page, average_salary_per_page,
-            number_of_pages, vacancies_found)
-
-
-def predict_average_rub_salary_all_pages(
-        input_function_name_of_predict_average_rub_salary_per_page,
-        programming_language):
-
-    vacancies_processed = 0
-    sum_predicted_rub_salary_all_pages = 0
+    salaries = []
     for page in count(0):
-        (vacancy_numbers_per_page, average_salary_per_page,
-         number_of_pages, vacancies_found) = \
-            input_function_name_of_predict_average_rub_salary_per_page(
-                programming_language,
-                page)
-        sum_predicted_rub_salary_all_pages += average_salary_per_page
-        vacancies_processed += vacancy_numbers_per_page
+        url = 'https://api.hh.ru/vacancies'
+        params = {
+            'text': f'Программист{programming_language}',
+            'period': '30',
+            'area': '1',
+            'page': page
+        }
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        resp = response.json()
+        vacancies = resp['items']
+        for vacancy_number, vacancy in enumerate(vacancies):
+            try:
+                lower_salary = vacancies[vacancy_number]['salary']['from']
+                top_salary = vacancies[vacancy_number]['salary']['to']
+                currency = vacancies[vacancy_number]['salary']['currency']
+                predicted_salary = calculate_predict_salary(
+                    currency,
+                    lower_salary,
+                    top_salary,
+                    'RUR',
+                )
+                if predicted_salary != 0:
+                    salaries.append(predicted_salary)
+            except TypeError:
+                pass
+        number_of_pages = resp['pages']
         if page >= number_of_pages-1:
             break
-    average_salary_all_pages = int(sum_predicted_rub_salary_all_pages/(page+1))
+    vacancies_found = resp['found']
+    vacancies_processed = len(salaries)
+    average_salary_all_pages = int(sum(salaries) / vacancies_processed)
     return vacancies_found, vacancies_processed, average_salary_all_pages
 
 
-def predict_average_rub_salary_per_page_sj(programming_language, page=0):
+def predict_average_rub_salary_sj(programming_language):
 
-    url = 'https://api.superjob.ru/2.33/vacancies/'
-    headers = {'X-Api-App-Id': f'{super_job_secret_key}'}
-    params = {
-        'catalogues': '48',
-        'town': '4',
-        'page': page,
-        'keyword': f'{programming_language}'
-    }
-    response = requests.get(url, params=params, headers=headers)
-    response.raise_for_status()
-    sum_salary, error, predicted_salary, vacancy_number = 0, 0, 0, 0
-    resp = response.json()
-    vacancies = resp['objects']
-    for vacancy_number, vacancy in enumerate(vacancies):
-        lower_salary = vacancies[vacancy_number]['payment_from']
-        top_salary = vacancies[vacancy_number]['payment_to']
-        currency = vacancies[vacancy_number]['currency']
-        predicted_salary, error = calculate_predict_salary(
-            currency,
-            lower_salary,
-            top_salary,
-            'rub',
-            error
-        )
-        sum_salary += predicted_salary
-    number_of_pages = (resp['total']//20+1)
-    vacancy_numbers_per_page = vacancy_number + 1 - error
+    salaries = []
+    for page in count(0):
+        url = 'https://api.superjob.ru/2.33/vacancies/'
+        headers = {'X-Api-App-Id': f'{super_job_secret_key}'}
+        params = {
+            'catalogues': '48',
+            'town': '4',
+            'page': page,
+            'keyword': f'{programming_language}'
+        }
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+        resp = response.json()
+        vacancies = resp['objects']
+        for vacancy_number, vacancy in enumerate(vacancies):
+            lower_salary = vacancies[vacancy_number]['payment_from']
+            top_salary = vacancies[vacancy_number]['payment_to']
+            currency = vacancies[vacancy_number]['currency']
+            predicted_salary = calculate_predict_salary(
+                currency,
+                lower_salary,
+                top_salary,
+                'rub',
+                )
+            if predicted_salary != 0:
+                salaries.append(predicted_salary)
+        number_of_pages = (resp['total'] // 20 + 1)
+        if page >= number_of_pages-1:
+            break
     vacancies_found = resp['total']
-    try:
-        average_salary_per_page = int(sum_salary/vacancy_numbers_per_page)
-    except ZeroDivisionError:
-        average_salary_per_page = 0
-    return (vacancy_numbers_per_page, average_salary_per_page,
-            number_of_pages, vacancies_found)
+    vacancies_processed = len(salaries)
+    average_salary_all_pages = int(sum(salaries)/vacancies_processed)
+    return vacancies_found, vacancies_processed, average_salary_all_pages
 
 
 def calculate_predict_salary(
-        currency, lower_salary, top_salary, valid_currency, error=0):
+        currency, lower_salary, top_salary, valid_currency):
 
     predicted_salary = 0
     if currency != valid_currency or (top_salary == 0 and lower_salary == 0):
-        error = error+1
+        pass
     elif top_salary == 0 or top_salary == None:
         predicted_salary = lower_salary*1.2
     elif lower_salary == 0 or lower_salary == None:
         predicted_salary = top_salary*0.8
     else:
         predicted_salary = (lower_salary+top_salary)/2
-    return predicted_salary, error
+    return predicted_salary
 
 
 def predict_average_salary_in_table_form_all_pages(title, dict_input):
@@ -189,30 +167,14 @@ if __name__ == '__main__':
     super_job_secret_key = os.getenv("SUPER_JOB_SECRET_KEY")
 
     try:
-        js_1, js_2, js_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'Javascript')
-        java_1, java_2, java_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'Javascript')
-        python_1, python_2, python_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'Python')
-        ruby_1, ruby_2, ruby_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'Ruby')
-        php_1, php_2, php_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'PHP')
-        scala_1, scala_2, scala_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'Scala')
-        c_1, c_2, c_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'C')
-        shell_1, shell_2, shell_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_hh,
-            'Shell')
+        js_1, js_2, js_3 = predict_average_rub_salary_hh('Javascript')
+        java_1, java_2, java_3 = predict_average_rub_salary_hh('Java')
+        python_1, python_2, python_3 = predict_average_rub_salary_hh('Python')
+        ruby_1, ruby_2, ruby_3 = predict_average_rub_salary_hh('Ruby')
+        php_1, php_2, php_3 = predict_average_rub_salary_hh('PHP')
+        scala_1, scala_2, scala_3 = predict_average_rub_salary_hh('Scala')
+        c_1, c_2, c_3 = predict_average_rub_salary_hh('C')
+        shell_1, shell_2, shell_3 = predict_average_rub_salary_hh('Shell')
 
         average_salary_of_vacancies_all_pages_hh = {
             'Javascript': {
@@ -257,30 +219,14 @@ if __name__ == '__main__':
             }
         }
 
-        js_1, js_2, jst_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'Javascript')
-        java_1, java_2, java_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'Javascript')
-        python_1, python_2, python_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'Python')
-        ruby_1, ruby_2, ruby_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'Ruby')
-        php_1, php_2, php_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'PHP')
-        scala_1, scala_2, scala_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'Scala')
-        c_1, c_2, c_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'C')
-        shell_1, shell_2, shell_3 = predict_average_rub_salary_all_pages(
-            predict_average_rub_salary_per_page_sj,
-            'Shell')
+        js_1, js_2, js_3 = predict_average_rub_salary_sj('Javascript')
+        java_1, java_2, java_3 = predict_average_rub_salary_sj('Java')
+        python_1, python_2, python_3 = predict_average_rub_salary_sj('Python')
+        ruby_1, ruby_2, ruby_3 = predict_average_rub_salary_sj('Ruby')
+        php_1, php_2, php_3 = predict_average_rub_salary_sj('PHP')
+        scala_1, scala_2, scala_3 = predict_average_rub_salary_sj('Scala')
+        c_1, c_2, c_3 = predict_average_rub_salary_sj('C')
+        shell_1, shell_2, shell_3 = predict_average_rub_salary_sj('Shell')
 
         average_salary_of_vacancies_all_pages_sj = {
             'Javascript': {
